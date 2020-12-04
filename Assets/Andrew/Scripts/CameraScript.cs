508c583ad492731e3390d235382f32f0f6cdf3b6
Andrew1707227 <1707227@mukilteo.wednet.edu>
2020-12-03 23:32:27 -0800

    Andrew: Created camera shake; added Parallax to the scene.

--- Assets/Andrew/Scripts/CameraScript.cs
+++ Assets/Andrew/Scripts/CameraScript.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour { 
     public GameObject player;
     public Vector3 offset;
+    public bool testButton;
+
+    public float CamShakeAmount;
+    public float CamShakeDuration;
+    private float ShakeRemaining;
 
     // Start is called before the first frame update
     void Start() {
-        
+        ShakeRemaining = 0;
     }
 
     // Update is called once per frame
     void LateUpdate() {
         transform.position = player.transform.position + offset;
+        CameraShake();
+        if (testButton) {
+            StartShake();
+        }
+    }
+
+    private void StartShake() {
+        ShakeRemaining = CamShakeDuration;
+    }
+
+    private void CameraShake() {
+        if (ShakeRemaining <= 0) return;
+            testButton = false;
+            // Adds a random Vector3 to the cam pos
+            transform.position = player.transform.position + offset + Random.insideUnitSphere * CamShakeAmount;
+            ShakeRemaining -= Time.deltaTime;
     }
 }
 
--- Assets/Andrew/Scripts/ParallaxScript.cs
+++ Assets/Andrew/Scripts/ParallaxScript.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParallaxTest : MonoBehaviour {
+public class ParallaxScript : MonoBehaviour {
 
     private float length, startPos;
     public GameObject cam;
     public float parallaxAmount;
+    public GameObject player;
 
     // Start is called before the first frame update
     void Start() {
@@ -19,7 +20,7 @@ public class ParallaxTest : MonoBehaviour {
         float temp = (cam.transform.position.x * (1 - parallaxAmount));
         float dist = (cam.transform.position.x * parallaxAmount);
 
-        transform.position = new Vector3(startPos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startPos + dist, player.transform.position.y, transform.position.z);
 
         if (temp > startPos + length) startPos += length;
         else if (temp < startPos - length) startPos -= length;
