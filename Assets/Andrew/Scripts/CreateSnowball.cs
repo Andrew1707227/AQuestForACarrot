28362693b0fc1710e8506595d44d8cf9191fb948
Andrew1707227 <1707227@mukilteo.wednet.edu>
2020-12-08 16:09:53 -0800

    Finished code for throwing snowball.

--- Assets/Andrew/Scripts/CreateSnowball.cs
+++ Assets/Andrew/Scripts/CreateSnowball.cs
@@ -8,12 +8,18 @@ public class CreateSnowball : MonoBehaviour {
 
     public float CoolDown;
     private float TimeRemaining;
+
+    private AudioSource ASource;
+    public Texture2D crosshair;
+    public Vector2 crosshairPos;
    
     // Start is called before the first frame update
     void Start() {
         SnowballTemplate.transform.position = new Vector3(9999, 9999); //im not a bad coder you are
         SnowballTemplate.GetComponent<Rigidbody2D>().gravityScale = 0;
+        ASource = GetComponent<AudioSource>();
         TimeRemaining = CoolDown;
+        Cursor.SetCursor(crosshair, crosshairPos, CursorMode.Auto);
     }
 
     // Update is called once per frame
@@ -22,7 +28,8 @@ public class CreateSnowball : MonoBehaviour {
         
         if (Input.GetButton("Fire1") && TimeRemaining >= CoolDown) {
             TimeRemaining = 0;
-            GetComponent<AudioSource>().Play();
+            ASource.pitch = Random.value / 5 + .9f;
+            ASource.Play();
             GameObject Snowball = Instantiate(SnowballTemplate);
             Snowball.name = "SnowballClone";
         }
--- Assets/Andrew/Scripts/SnowballLogic.cs
+++ Assets/Andrew/Scripts/SnowballLogic.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class SnowballLogic : MonoBehaviour {
 
     public float airTime;
-    public Vector2 force;
+    private Vector2 force;
+    public float scalar;
 
     private float timer;
     private Rigidbody2D rb2;
 
     public GameObject player;
     private SpriteRenderer sr;
+
     private bool direction;
     public Vector3 offset; //So the snowball is in player's hand
 
@@ -30,15 +32,22 @@ public class SnowballLogic : MonoBehaviour {
     void Start() {
         rb2 = GetComponent<Rigidbody2D>();
         sr = player.GetComponent<SpriteRenderer>();
+
+        Vector3 playerPos = player.transform.position;
+        Vector3 mousePos = Input.mousePosition;
+
         direction = sr.flipX;
         timer = 0;
+
         if (gameObject.name == "SnowballClone") {
             rb2.gravityScale = 1;
             if (!direction) {
-                transform.position = player.transform.position + new Vector3(-offset.x,offset.y,offset.z);
+                transform.position = playerPos + new Vector3(-offset.x,offset.y,offset.z);
             } else {
-                transform.position = player.transform.position + offset;
-            } 
+                transform.position = playerPos + offset;
+            }
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            force = (mousePos - playerPos) * scalar;
         }
     }
 
@@ -46,11 +55,11 @@ public class SnowballLogic : MonoBehaviour {
         if (gameObject.name == "SnowballClone") { 
             timer += Time.deltaTime;
             if (timer < airTime) {
-                if (!direction) {
+                /*if (!direction) {
                     rb2.AddForce(new Vector2(-force.x, force.y));
-                } else {
+                } else {*/
                     rb2.AddForce(force);
-                }
+                //}
             }
             if(transform.position.y < offMap) {
                 Destroy(gameObject);
