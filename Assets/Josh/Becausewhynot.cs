666bb4c8ead5be036fd567e804720bf7bfe9d1a8
Joshypooh <1707229@mukilteo.wednet.edu>
2020-12-28 14:31:09 -0800

    goomba thing

---
+++ Assets/Josh/Becausewhynot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Becausewhynot : MonoBehaviour
+{
+    void MovePlayer()
+    {
+        gameObject.transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+    public void KillPLayer()
+    {
+        Destroy(this.gameObject);
+    }
+}
