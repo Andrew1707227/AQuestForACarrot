69c856fccd7f43901ce933f7aa77ffbd35135956
Joshypooh <1707229@mukilteo.wednet.edu>
2021-01-06 12:45:35 -0800

    Update Goomba.cs and joshy's stuff.unity

--- Assets/Josh/Goomba.cs
+++ Assets/Josh/Goomba.cs
@@ -6,7 +6,7 @@ public class Goomba : MonoBehaviour
 {
     public GameObject Enemy;
     private GameObject Snowball;
-
+    private GameObject Player;
     //To check out where its going
     bool movingDirection = false;
 
@@ -17,22 +17,12 @@ public class Goomba : MonoBehaviour
         transform.Translate(((movingDirection ? -1 : 1) * Vector3.left * Time.deltaTime * speed));
     }
 
-    void OnCollisionEnter2D(Collision2D other)
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Edge" || other.gameObject.tag == "Enemy")
         {
             //Reverse Direction please
             movingDirection = !movingDirection;
-        }
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            //Being Killed
-            Destroy(this.gameObject);
-            //AddScore
         }
     }
 }
