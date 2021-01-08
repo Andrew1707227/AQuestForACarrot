8687073ecbc265c773c5f96621b6e30f8e9f3331
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-01-08 01:00:01 -0800

    Added checkpoint flags, bunny jump, and additional map pieces

---
+++ Assets/Andrew/Scripts/BunnyAnimator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyAnimator : MonoBehaviour {
+
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    private float timeRemaining = 0;
+    private Vector3 prevTransform;
+
+    // Start is called before the first frame update
+    void Start() {
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()  {
+        sr.flipX = transform.position.x > prevTransform.x;
+        anim.Play("BunnyJump");
+        prevTransform = transform.position;
+    }
+}
--- Assets/Andrew/Scripts/CheckpointDetector.cs
+++ Assets/Andrew/Scripts/CheckpointDetector.cs
@@ -6,14 +6,18 @@ public class CheckpointDetector : MonoBehaviour {
 
     public bool activated;
     public GameObject Player;
+    private Animator anim;
     
     void Start() {
         activated = false;
+        anim = GetComponent<Animator>();
     }
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.tag == "Player" && !activated) {
+            anim.enabled = true;
             activated = true;
             Player.GetComponent<PlayerHealthScript>().Checkpoint = gameObject;
+            GetComponent<AudioSource>().Play();
         }
     }
 }
