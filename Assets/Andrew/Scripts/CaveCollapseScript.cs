a78be9ba40ad98f5a352acb8573d83da96d8ef86
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-01-09 13:13:10 -0800

    Added boss theme and a trigger for boss theme

---
+++ Assets/Andrew/Scripts/CaveCollapseScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaveCollapseScript : MonoBehaviour {
+
+    public GameObject Music;
+    private bool debounce = false;
+    private AudioSource Asource;
+    // Start is called before the first frame update
+    void Start() {
+        Asource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()  {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag == "Player" && !debounce) {
+            debounce = true;
+            Music.GetComponent<AudioSource>().enabled = false;
+            Asource.Play();
+        }
+    }
+}
