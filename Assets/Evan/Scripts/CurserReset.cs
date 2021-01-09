1c174ea540403e32f3e80b46c887ed94dfacaa29
Evan <1707224@mukilteo.wednet.edu>
2021-01-08 16:19:25 -0800

    EG: Main menu curser fix

---
+++ Assets/Evan/Scripts/CurserReset.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurserReset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Sets cursor to default
+        Cursor.SetCursor(null, new Vector2(), CursorMode.Auto);
+    }
+}
