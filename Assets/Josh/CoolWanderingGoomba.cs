1a0cdd2d18886735d5183b9f73482e5dfc84d048
Joshypooh <1707229@mukilteo.wednet.edu>
2021-01-08 18:08:05 -0800

    Update CoolWanderingGoomba.cs, CoolWanderingGoomba.cs.meta, and 2 more files...

---
+++ Assets/Josh/CoolWanderingGoomba.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoolWanderingGoomba : MonoBehaviour
+{// The movement speed of the object
+    public float moveSpeed = 0.2f;
+    bool overkill = false;
+    bool movingDirection = false;
+    // A minimum and maximum time delay for taking a decision, choosing a direction to move in
+    public Vector2 decisionTime = new Vector2(1, 4);
+    internal float decisionTimeCount = 0;
+
+    // The possible directions that the object can move int, right, left, up, down, and zero for staying in place. I added zero twice to give a bigger chance if it happening than other directions
+    internal Vector3[] moveDirections = new Vector3[] { Vector3.right, Vector3.zero, Vector3.left, Vector3.zero };
+    internal int currentMoveDirection = 0;
+    // Start is called before the first frame update
+    void ChooseMoveDirection()
+    {
+        currentMoveDirection++;
+        currentMoveDirection = currentMoveDirection % moveDirections.Length;
+    }
+    void Start()
+    {
+        // Cache the transform for quicker access
+
+        // Set a random time delay for taking a decision ( changing direction, or standing in place for a while )
+        decisionTimeCount = Random.Range(decisionTime.x, decisionTime.y);
+
+        // Choose a movement direction, or stay in place
+        ChooseMoveDirection();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the object in the chosen direction at the set speed
+       transform.position += moveDirections[currentMoveDirection] * Time.deltaTime * moveSpeed;
+
+        if (decisionTimeCount > 0) decisionTimeCount -= Time.deltaTime;
+        else
+        {
+            // Choose a random time delay for taking a decision ( changing direction, or standing in place for a while )
+            decisionTimeCount = Random.Range(decisionTime.x, decisionTime.y);
+
+            // Choose a movement direction, or stay in place
+            ChooseMoveDirection();
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+          if (collision.tag == "Edge" || collision.tag == "Enemy")
+            {
+                //Reverse Direction please
+                movingDirection = !movingDirection;
+                //i like bread
+            }
+    }
+}
--- Assets/Josh/Goomba.cs
+++ Assets/Josh/Goomba.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class Goomba : MonoBehaviour
 {
+    private void Start()
+    {
+
+    
+    }
     public GameObject Enemy;
     private GameObject Snowball;
     private GameObject Player;
     //To check out where its going
     bool movingDirection = false;
+    public float Duration;
+    public Vector2 freeze = new Vector2(1, 2);
 
     //Goomba Speed
-    [SerializeField] float speed;
+    [SerializeField] float Distance;
     void Update()
     {
-        transform.Translate(((movingDirection ? -1 : 1) * Vector3.left * Time.deltaTime * speed));
+        //transform.Translate(((movingDirection ? -1 : 1) * Vector3.left * Time.deltaTime * speed));
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -23,6 +30,7 @@ public class Goomba : MonoBehaviour
         {
             //Reverse Direction please
             movingDirection = !movingDirection;
+            //i like bread
         }
     }
 }
