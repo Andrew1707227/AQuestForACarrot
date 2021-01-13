3366c9361cd48d73e923c4c57c6b7c0716a39cbc
Andrew1707227 <1707227@mukilteo.wednet.edu>
2021-01-13 14:21:12 -0800

    Finally fixed boss bunny spawner, added bunny roar/death sfx, bug fixes

--- Assets/Andrew/Scripts/BossScript.cs
+++ Assets/Andrew/Scripts/BossScript.cs
@@ -6,40 +6,49 @@ public class BossScript : MonoBehaviour {
 
     public GameObject carrot;
     public GameObject Boss;
-    public GameObject BossHealthBar;
 
     private GameObject BossClone;
     private bool debounce;
     private Vector3 bossPos;
 
     private bool isDead;
+    private bool isPlayerDead;
     private float timer;
+    private float CarrotTimer;
 
     void Start() {
         carrot.SetActive(false);
         Boss.SetActive(false);
         isDead = false;
+        CarrotTimer = 0;
         timer = 0;
     }
 
     // Update is called once per frame
     void Update() {
-        if (BossClone == null) return;
-        if (BossClone.GetComponent<BossBunnyHealth>().getLives() <= 0) {
-            isDead = true;
-        } else {
-            bossPos = BossClone.transform.position;
+        if (BossClone != null) { 
+            if (BossClone.GetComponent<BossBunnyHealth>().getLives() <= 0) {
+                isDead = true;
+            } else {
+                bossPos = BossClone.transform.position;
+            }
         }
-        if (isDead) timer += Time.deltaTime;
-        if (timer > 2 && timer < 2.1) {
+        if (isDead) CarrotTimer += Time.deltaTime;
+        if (CarrotTimer > 2 && CarrotTimer < 2.1) {
             carrot.SetActive(true);
             carrot.transform.position = bossPos + new Vector3(-2,2,0);
         }
         if (PlayerHealthScript.GetLives() <= 0) {
             //reset the boss trigger
-            BossHealthBar.SetActive(false);
-            if (BossClone != null) Destroy(BossClone);
-            debounce = false;
+            isPlayerDead = true;
+            if (BossClone != null) debounce = false;
+        } else {
+            isPlayerDead = false;
+        }
+        if (isPlayerDead && BossClone != null) timer += Time.deltaTime;
+        if (timer > 2 && BossClone != null && !isPlayerDead) {
+            Destroy(BossClone);
+            timer = 0;
         }
     }
     private void OnTriggerEnter2D(Collider2D collision) {
@@ -48,7 +57,6 @@ public class BossScript : MonoBehaviour {
             BossClone = Instantiate(Boss);
             BossClone.name = "BossBun";
             BossClone.SetActive(true);
-            BossHealthBar.SetActive(true);
         }
     }
 }
