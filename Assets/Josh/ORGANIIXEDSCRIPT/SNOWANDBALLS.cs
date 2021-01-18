fa11bcc33866e32b9115815375ed9c16db510896
Joshypooh <1707229@mukilteo.wednet.edu>
2021-01-17 20:48:25 -0800

    Update Becausewhynot.cs, Becausewhynot.cs.meta, and 20 more files...

---
+++ Assets/Josh/ORGANIIXEDSCRIPT/SNOWANDBALLS.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SNOWANDBALLS : MonoBehaviour
+{
+    public GameObject SnowballTemplate;
+    public Sprite OongaBoonga;
+    public float CoolDown;
+    private float Timer;
+    
+    private AudioSource ASource;
+    private SpriteRenderer sr;
+    private Animator ani;
+
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Timer = 0;
+        SnowballTemplate.transform.position = new Vector3(9999, 9999); //im not a bad coder you are
+        SnowballTemplate.GetComponent<Rigidbody2D>().gravityScale = 0;
+
+        ASource = GetComponent<AudioSource>();
+        sr = GetComponent<SpriteRenderer>();
+        ani = GetComponent<Animator>();
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* if (TimeRemaining <= CoolDown) TimeRemaining += Time.deltaTime;
+         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+         float dx = transform.position.x - mousePos.x;
+         bool flip = sr.flipX;
+
+         if ((dx > 0 && !flip || dx < 0 && flip) && TimeRemaining >= CoolDown && !PauseMenu.gameIsPaused)
+         {
+             if (Input.GetButton("Fire1") && TimeRemaining >= CoolDown)
+             {
+                 TimeRemaining = 0;
+                 ASource.pitch = Random.value / 5 + .9f;
+                 ASource.Play();
+                 ani.SetTrigger("aniThrow");
+                 GameObject Snowball = Instantiate(SnowballTemplate);
+                 Snowball.name = "SnowballClone";
+             }
+         }*/
+        if (CoolDown <= 0)
+        {
+                        ani.enabled = true;
+            CoolDown = 1.5f;
+        }
+        CoolDown -= Time.deltaTime;
+        if (!ani.enabled) sr.sprite = OongaBoonga;
+        if (ani.enabled) Timer += Time.deltaTime;
+        if (Timer > .75 && Timer < .77)
+        {
+            ASource.pitch = Random.value / 5 + .9f;
+            ASource.Play();
+
+            GameObject Snowball = Instantiate(SnowballTemplate);
+            Snowball.name = "SnowballClone";
+            
+        }
+        if (Timer > 1)
+        {
+            ani.enabled = false;
+            Timer = 0;
+        }
+    }
+}
---
+++ Assets/Josh/ORGANIIXEDSCRIPT/SNOWANDBALLSWITHLOGIC.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SNOWANDBALLSWITHLOGIC : MonoBehaviour
+{
+
+    public float airTime;
+    private Vector2 force;
+    public float scalar;
+
+    private float timer;
+    private Rigidbody2D rb2;
+
+    public GameObject player;
+    public GameObject Baller;
+    private SpriteRenderer sr;
+
+    private bool direction;
+    public Vector3 offset; //So the snowball is in player's hand
+    private ParticleSystem PS;
+
+    public float offMap;
+    private bool isHit = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((collision.gameObject.layer == 8 /* 8 = ground layer */ || collision.tag == "Player") && !isHit)
+        {
+            isHit = true;
+            GetComponent<AudioSource>().Play();
+            PS.Play();
+            rb2.velocity = new Vector2();
+            GetComponent<SpriteRenderer>().enabled = false;
+            Destroy(GetComponent<TrailRenderer>());
+        }
+    }
+
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        sr = player.GetComponent<SpriteRenderer>();
+        PS = GetComponent<ParticleSystem>();
+
+        Vector3 playerPos = player.transform.position;
+        Vector3 BallerPos = Baller.transform.position;
+
+        direction = sr.flipX;
+        timer = 0;
+
+        if (gameObject.name == "SnowballClone")
+        {
+            rb2.gravityScale = 1;
+            if (!direction)
+            {
+                transform.position = BallerPos + new Vector3(-offset.x, offset.y, offset.z);
+            }
+            else
+            {
+                transform.position = playerPos + offset;
+            }
+            force = (playerPos - BallerPos) * scalar;
+            rb2.AddForce(force, ForceMode2D.Impulse);
+        }
+    }
+
+    void Update()
+    {
+        if (gameObject.name == "SnowballClone")
+        {
+            timer += Time.deltaTime;
+            if (timer < airTime)
+            {
+                // rb2.AddForce(force, ForceMode2D.Impulse);
+            }
+            if (transform.position.y < offMap)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
